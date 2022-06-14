c2ca68a0fd72c8a40cf36e9c17b7bf91c1642612
hanan <hananeanghari1@gmail.com>
2022-06-15 00:20:38 +0200

    feat(mediatheque-hanan): developpement de product-list :tada:

--- src/app/pages/product-page/product-list/product-list.component.ts
+++ src/app/pages/product-page/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
+  @Input() productCardList:any;
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
 
 }
